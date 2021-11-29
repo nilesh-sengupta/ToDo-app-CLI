00490ce12c15d25be60bca5b6715010cad2a31a1
Nilesh Sengupta <nileshsengupta43@gmail.com>
2021-11-29 21:17:05 +0530

    Update todo_app.py
    Added time left and overdue feature

--- todo_app.py
+++ todo_app.py
@@ -1,19 +1,37 @@
 import typer
-
+import datetime
+from datetime import date
 app=typer.Typer()
 @app.command()
 def add(t: str):
-    t=t+'\n'
+    time=''
+    time = typer.prompt("deadline?(dd/mm/yyyy)")
+    at=''
+    at = typer.prompt("at?(HH:MM)")
+    t=time+" at "+at+" "+t+'\n'
     file1=open("tasks.txt",'a')
     file1.write(t)
     file1.close()
 @app.command()
 def read():
+    today1 = datetime.datetime.now()
     file1=open("tasks.txt",'r')
     res=file1.readlines()
     file1.close()
     for i in range(0,len(res)):
+        u=res[i]
+        d=int(u[0]+u[1])
+        m=int(u[3]+u[4])
+        y=int(u[6]+u[7]+u[8]+u[9])
+        h=int(u[14]+u[15])
+        min=int(u[17]+u[18])
+        now=datetime.datetime(y,m,d,h,min)
+        lefttime=str(now-today1)
         print((i+1),". ",res[i])
+        if(lefttime[0]=="-"):
+            print("Overdue")
+        else:
+            print("Time left is:",lefttime)
 @app.command()
 def delete(num: int):
     with open(r"tasks.txt", 'r+') as fp:
